6f5460d984e2af188ae38fbebe107dd55810e9de
VALKYRIEXB7 <samuelpotter223@gmail.com>
2025-04-25 10:54:48 -0400

    finished fillWords method

--- WordGame.java
+++ WordGame.java
@@ -17,6 +17,8 @@ public class WordGame {
 
     public void fillWords() {
         /* adds basic list of letter-respective words to ArrayList */
+
+        /* adds three letter words to ArrayList */
         letter3.add("cat");
         letter3.add("dog");
         letter3.add("bat");
@@ -32,5 +34,124 @@ public class WordGame {
         letter3.add("bet");
         letter3.add("fan");
         letter3.add("sit");
+
+        /* adds four letter words to ArrayList */
+        letter4.add("word");
+        letter4.add("play");
+        letter4.add("game");
+        letter4.add("book");
+        letter4.add("time");
+        letter4.add("life");
+        letter4.add("work");
+        letter4.add("home");
+        letter4.add("love");
+        letter4.add("mind");
+        letter4.add("hand");
+        letter4.add("head");
+        letter4.add("face");
+        letter4.add("body");
+        letter4.add("soul");
+
+        /* adds five letter words to ArrayList */
+        letter5.add("world");
+        letter5.add("happy");
+        letter5.add("music");
+        letter5.add("dream");
+        letter5.add("peace");
+        letter5.add("heart");
+        letter5.add("light");
+        letter5.add("smile");
+        letter5.add("brain");
+        letter5.add("space");
+        letter5.add("earth");
+        letter5.add("water");
+        letter5.add("power");
+        letter5.add("story");
+        letter5.add("friend");
+
+        /* adds six letter words to ArrayList */
+        letter6.add("beauty");
+        letter6.add("nature");
+        letter6.add("wisdom");
+        letter6.add("freedom");
+        letter6.add("spirit");
+        letter6.add("garden");
+        letter6.add("window");
+        letter6.add("summer");
+        letter6.add("winter");
+        letter6.add("spring");
+        letter6.add("autumn");
+        letter6.add("memory");
+        letter6.add("shadow");
+        letter6.add("silver");
+        letter6.add("golden");
+
+        /* adds seven letter words to ArrayList */
+        letter7.add("journey");
+        letter7.add("freedom");
+        letter7.add("wonder");
+        letter7.add("courage");
+        letter7.add("diamond");
+        letter7.add("rainbow");
+        letter7.add("morning");
+        letter7.add("evening");
+        letter7.add("forever");
+        letter7.add("silence");
+        letter7.add("whisper");
+        letter7.add("dancing");
+        letter7.add("singing");
+        letter7.add("dreaming");
+        letter7.add("shining");
+
+        /* adds eight letter words to ArrayList */
+        letter8.add("adventure");
+        letter8.add("beautiful");
+        letter8.add("champion");
+        letter8.add("discover");
+        letter8.add("elephant");
+        letter8.add("fountain");
+        letter8.add("grateful");
+        letter8.add("harmony");
+        letter8.add("infinity");
+        letter8.add("journey");
+        letter8.add("kindness");
+        letter8.add("laughter");
+        letter8.add("mountain");
+        letter8.add("paradise");
+        letter8.add("rainbow");
+
+        /* adds nine letter words to ArrayList */
+        letter9.add("adventure");
+        letter9.add("beautiful");
+        letter9.add("butterfly");
+        letter9.add("chocolate");
+        letter9.add("discovery");
+        letter9.add("education");
+        letter9.add("friendship");
+        letter9.add("happiness");
+        letter9.add("infinite");
+        letter9.add("knowledge");
+        letter9.add("lightning");
+        letter9.add("mountain");
+        letter9.add("paradise");
+        letter9.add("rainbow");
+        letter9.add("sunshine");
+
+        /* adds ten letter words to ArrayList */
+        letter10.add("adventure");
+        letter10.add("beautiful");
+        letter10.add("butterfly");
+        letter10.add("chocolate");
+        letter10.add("discovery");
+        letter10.add("education");
+        letter10.add("friendship");
+        letter10.add("happiness");
+        letter10.add("infinity");
+        letter10.add("knowledge");
+        letter10.add("lightning");
+        letter10.add("mountain");
+        letter10.add("paradise");
+        letter10.add("rainbow");
+        letter10.add("sunshine");
     }
 }
