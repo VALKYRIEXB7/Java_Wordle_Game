0af0392234ef48ddda339d75dea08040f9843156
VALKYRIEXB7 <samuelpotter223@gmail.com>
2025-04-25 10:38:05 -0400

    Add files via upload

---
+++ WordGameRunner.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class WordGame {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String out = "";
+        System.out.print("Enter a String: ");
+        out = s.nextLine();
+        System.out.println(out);
+
+    }
+}
